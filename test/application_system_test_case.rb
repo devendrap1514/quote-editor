f3d87ab560eb145ac697875da4e5525148151f5d
Devendra Patidar <devendrap@shriffle.com>
2023-11-24 19:10:24 +0530

    intialize project and setup with database and implement test cases

---
+++ test/application_system_test_case.rb
@@ -0,0 +1,9 @@
+require "test_helper"
+
+class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+  driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
+
+  def custom_sleep(second)
+    sleep(second)
+  end
+end
